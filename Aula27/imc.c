426094b1266c145331b354cbd1458f5d9de8ab21
Julia Iglesias <byjuliaiglesias@gmail.com>
2025-02-25 10:56:13 -0300

    calculadora IMC

--- Aula27/imc.c
+++ Aula27/imc.c
@@ -16,15 +16,15 @@ void setup()
      Serial.println("magreza");
   }
      
-  else if(IMC <= 24.9 & IMC > 18.5){
+  else if(IMC <= 24.9 && IMC > 18.5){
      Serial.println("normal");
   }
  
-  else if(IMC <= 29.9 & IMC >= 25) {
+  else if(IMC <= 29.9 && IMC >= 25) {
      Serial.println("sobrepeso"); 
   }
      
-  else if(IMC <= 34.9 & IMC >= 30) {
+  else if(IMC <= 34.9 && IMC >= 30) {
      Serial.println("obesidade");
   }
      
